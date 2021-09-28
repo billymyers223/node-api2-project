c7cc00a503914346887f1e8f99baddcacbd12fe8
billmyers223 <billymyers223@gmail.com>
2021-09-28 15:25:10 -0500

    passing first 2 tests will finish soon

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
+
+router.get('/', async (req,res)=>{
+    
+        try{
+            const posts = await Post.find()
+            res.status(200).json(posts)
+        }
+        catch{
+            console.log('error')
+            res.status(500).json({
+                message:'Error retrieving posts',
+            })
+        }
+})
+module.exports = router
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// require your server and launch it here
+   
+const server = require('./api/server');
+
+
+
+server.listen(4000, () =>{
+    console.log('Server Listening on port 4000')
+})
+ 
